32351fbd40cf9dff49f2fc209141b032be42c4b6
Paul Knopf <theonlylawislove@gmail.com>
2012-12-22 22:19:06 -0800

    Added build/deploy process.

--- N2Bootstrap.Blog/Properties/AssemblyInfo.cs
+++ N2Bootstrap.Blog/Properties/AssemblyInfo.cs
@@ -13,23 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6cf4f6fe-1229-4825-93df-f008ea65c0e2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
