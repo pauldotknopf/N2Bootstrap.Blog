623423f8fe02f5ff57843c23bfbcce537ab05316
Paul Knopf <theonlylawislove@gmail.com>
2012-12-31 23:57:43 -0800

    Posts part now with configurable thumbnail size/visibility

--- N2Bootstrap.Blog.Library/Models/Posts.cs
+++ N2Bootstrap.Blog.Library/Models/Posts.cs
@@ -19,6 +19,15 @@ namespace N2Bootstrap.Blog.Library.Models
         [EditableLink(Title = "Blog", SortOrder = 202)]
         public virtual ContentItem Blog { get; set; }
 
+        [EditableCheckBox(Title = "Show image", CheckBoxText = "", SortOrder = 204)]
+        public virtual bool ShowImage { get; set; }
+
+        [EditableNumber(Title = "Image size", DefaultValue = 46, Required = true, MinimumValue = "16", MaximumValue = "96", InvalidRangeText = "Please specify a valid image size (16-96).", SortOrder = 205)]
+        public virtual int ImageSize { get; set; }
+
+        [EditableNumber(Title = "Maximum characters", DefaultValue = 140, Required = true, MinimumValue = "20", MaximumValue = "500", InvalidRangeText = "Please specify a valid number of characters (20-500).", SortOrder = 206)]
+        public virtual int MaxCharacters { get; set; }
+
         /// <summary>
         /// Get the bog container to render posts from
         /// </summary>
